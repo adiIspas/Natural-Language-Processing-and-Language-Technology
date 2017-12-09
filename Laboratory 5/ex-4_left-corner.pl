b24892fbc3533e11c8357f89c60e8e71ff82c142
Adrian Ispas <adrian.ispas@ab4.systems>
2017-12-09 16:42:43 +0200

    Align ...

--- Laboratory 5/ex-4_left-corner.pl
+++ Laboratory 5/ex-4_left-corner.pl
@@ -1,20 +1,20 @@
 % Laboratory 5
 
- parse(Categorie, [Cuvant | SirRamas], Sir) :-
-	 cuvant(Subcategorie, Cuvant),
-	 completeaza(Subcategorie, Categorie, SirRamas, Sir).
+parse(Categorie, [Cuvant | SirRamas], Sir) :-
+ cuvant(Subcategorie, Cuvant),
+ completeaza(Subcategorie, Categorie, SirRamas, Sir).
 
- parse_lista([Categorie | Categorii], Sir1, Sir) :-
-	 parse(Categorie, Sir1, Sir2),
-	 parse_lista(Categorii, Sir2, Sir).
+parse_lista([Categorie | Categorii], Sir1, Sir) :-
+ parse(Categorie, Sir1, Sir2),
+ parse_lista(Categorii, Sir2, Sir).
 
- parse_lista([], Sir, Sir).
+parse_lista([], Sir, Sir).
 
- completeaza(Subcategorie, Subcategorie, Sir, Sir).
- completeaza(Subcategorie, Categorie, Sir1, Sir) :-
-	 regula(Parinte, [Subcategorie | Rest]),
-	 parse_lista(Rest, Sir1, Sir2),
-	 completeaza(Parinte, Categorie, Sir2, Sir).
+completeaza(Subcategorie, Subcategorie, Sir, Sir).
+completeaza(Subcategorie, Categorie, Sir1, Sir) :-
+ regula(Parinte, [Subcategorie | Rest]),
+ parse_lista(Rest, Sir1, Sir2),
+ completeaza(Parinte, Categorie, Sir2, Sir).
 
 regula(s, [np, vp]).
 regula(np, [det, n]).
