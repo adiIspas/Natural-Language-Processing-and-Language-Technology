6733124c4d1fd844b80b00c0dac212f4a9d426d7
Adrian Ispas <adrian.ispas@ab4.systems>
2017-12-09 16:37:06 +0200

    Finished ex 4.

--- Laboratory 5/ex-4_left-corner.pl
+++ Laboratory 5/ex-4_left-corner.pl
@@ -1,67 +1,46 @@
 % Laboratory 5
 
-% parse(Categorie, [Cuvant | SirRamas], Sir) :-
-	% cuvant(Subcategorie, Cuvant),
-	% completeaza(Subcategorie, Categorie, SirRamas, Sir).
+ parse(Categorie, [Cuvant | SirRamas], Sir) :-
+	 cuvant(Subcategorie, Cuvant),
+	 completeaza(Subcategorie, Categorie, SirRamas, Sir).
 
-% parse_lista([Categorie | Categorii], Sir1, Sir) :-
-	% parse(Categorie, Sir1, Sir2),
-	% parse_lista(Categorii, Sir2, Sir).
+ parse_lista([Categorie | Categorii], Sir1, Sir) :-
+	 parse(Categorie, Sir1, Sir2),
+	 parse_lista(Categorii, Sir2, Sir).
 
-% parse_lista([], Sir, Sir).
+ parse_lista([], Sir, Sir).
 
-% completeaza(Subcategorie, Subcategorie, Sir, Sir).
-% completeaza(Subcategorie, Categorie, Sir1, Sir) :-
-	% regula(Parinte, [Subcategorie | Rest]),
-	% parse_lista(Rest, Sir1, Sir2),
-	% completeaza(Parinte, Categorie, Sir2, Sir).
-
-% regula(s, [np, vp]).
-% regula(np, [det, n]).
-% regula(np, [np, conj, np]).
-% regula(vp, [v, np]).
-% regula(vp, [v, np, pp]).
-% regula(pp, [p, np]).
-
-% cuvant(det, the).
-% cuvant(det, all).
-% cuvant(det, every).
-% cuvant(p, near).
-% cuvant(conj, and).
-% cuvant(n, dog).
-% cuvant(n, dogs).
-% cuvant(n, cat).
-% cuvant(n, cats).
-% cuvant(n, elephant).
-% cuvant(n, elephants).
-% cuvant(v, chase).
-% cuvant(v, chases).
-% cuvant(v, see).
-% cuvant(v, sees).
-% cuvant(v, amuse).
-% cuvant(v, amuses).
-
-parse(C, [Cuvant|S2], S) :- cuvant(W, Cuvant), completeaza(W,C,S2,S).
-
-parse_lista([C|Cs], S1, S) :- parse(C, S1, S2), parse_lista(Cs, S2, S).
-parse_lista([], S, S).
-
-completeaza(C,C,S,S).
-completeaza(W,C,S1,S):-regula(P,[W|Rest]),parse_lista(Rest,S1,S2),completeaza(P,C,S2,S).
+ completeaza(Subcategorie, Subcategorie, Sir, Sir).
+ completeaza(Subcategorie, Categorie, Sir1, Sir) :-
+	 regula(Parinte, [Subcategorie | Rest]),
+	 parse_lista(Rest, Sir1, Sir2),
+	 completeaza(Parinte, Categorie, Sir2, Sir).
 
 regula(s, [np, vp]).
-regula(vp, [v, np]).
 regula(np, [det, n]).
+regula(np, [np, conj, np]).
+regula(vp, [v, np]).
+regula(vp, [v, np, pp]).
+regula(pp, [p, np]).
 
-cuvant(det, un).
-cuvant(det, niste).
-cuvant(det, o).
-cuvant(n, elev).
-cuvant(n, elevi).
-cuvant(n, carte).
-cuvant(v, iubeste).
-cuvant(v, iubesc).
+cuvant(det, the).
+cuvant(det, all).
+cuvant(det, every).
+cuvant(p, near).
+cuvant(conj, and).
+cuvant(n, dog).
+cuvant(n, dogs).
+cuvant(n, cat).
+cuvant(n, cats).
+cuvant(n, elephant).
+cuvant(n, elephants).
+cuvant(v, chase).
+cuvant(v, chases).
+cuvant(v, see).
+cuvant(v, sees).
+cuvant(v, amuse).
+cuvant(v, amuses).
 
-% Exemplu apel: general_parse([niste, elevi, iubesc, o, carte]). - propozitia
+% Exemplu apel: general_parse([the, dog, see, the, cat]). - propozitia
 % trebuie sa fie conforma cu gramatica pentru un raspuns yes.
 general_parse(X) :- parse(s, X, []).
