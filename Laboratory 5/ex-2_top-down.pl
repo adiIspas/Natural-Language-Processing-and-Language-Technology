694049d268b87f3d2529688136705ccb05eec15e
Adrian Ispas <adrian.ispas@ab4.systems>
2017-12-28 12:54:30 +0200

    Update rules.

--- Laboratory 5/ex-2_top-down.pl
+++ Laboratory 5/ex-2_top-down.pl
@@ -12,18 +12,18 @@ parse_lista([C|Categorii], S1, S, [A|Arbore]) :-
 	parse_lista(Categorii, S2, S, Arbore).
 parse_lista([], S, S, []).
 
-regula(s, [np, vp]).
-regula(vp, [v]).
-regula(vp, [v, np]).
-regula(np, [n]).
-regula(np, [det, n]).
+regula(s,[np,vp]).
+regula(vp,[v,np]).
+regula(np,[det,n]).
 
-cuvant(n, ana).
-cuvant(n, mere).
-cuvant(n, baietii).
-cuvant(v, are).
-cuvant(v, au).
-cuvant(det, multe).
+cuvant(det,un).
+cuvant(det,niste).
+cuvant(det,o).
+cuvant(n,elev).
+cuvant(n,elevi).
+cuvant(n,carte).
+cuvant(v,iubeste).
+cuvant(v,iubesc).
 
 % Exemplu apel: parse_sentence(X, A). - va genera rand pe rand cate o combinatie
 parse_sentence(X, Arbore) :- parse(s, X, [], Arbore).
