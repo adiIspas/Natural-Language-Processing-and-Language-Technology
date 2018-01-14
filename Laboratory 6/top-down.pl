85f71a666ed9d9a4eb6854b6197f5655728d9610
Adrian Ispas <adrian.ispas@ab4.systems>
2018-01-14 22:26:34 +0200

    Finished laboratory 6.

--- Laboratory 6/top-down.pl
+++ Laboratory 6/top-down.pl
@@ -1,11 +1,11 @@
 % Top-down cu harta si agenda.
 
-:- dynamic muchie / 4 .
+:- dynamic muchie / 5 .
 
 init_agenda([], _, []).
 init_agenda([Cuvant|Cuvinte], V0, Agenda) :-
   V1 is V0 + 1,
-  findall(muchie(V0, V1, Categorie, []), cuvant(Categorie, Cuvant), Agenda1),
+  findall(muchie(V0, V1, Categorie, [], [Cuvant, Categorie]), cuvant(Categorie, Cuvant), Agenda1),
   init_agenda(Cuvinte, V1, Agenda2),
   append(Agenda1, Agenda2, Agenda).
 
@@ -32,20 +32,21 @@ adauga_muchii([Muchie|Muchii],Muchii1,Muchii3) :-
   adauga_muchii(Muchii,Muchii2,Muchii3).
 
 init_activ(Categorie,Varf,Muchii) :-
-  findall(muchie(Varf,Varf,Categorie,Categorii),regula(Categorie,Categorii),Muchii).
+  findall(muchie(Varf,Varf,Categorie,Categorii,[Categorie]),regula(Categorie,Categorii),Muchii).
 
-muchii_noi(muchie(V1,V2,Categorie1,[]),Harta,Muchii) :-
-  findall(muchie(V0,V2,Categorie2,Categorii), membru(muchie(V0,V1,Categorie2,[Categorie1|Categorii]),Harta),Muchii).
-muchii_noi(muchie(V1,V2,Categorie1,[Categorie2|Categorii]),Harta,Muchii) :-
+muchii_noi(muchie(V1,V2,Categorie1,[],Analiza),Harta,Muchii) :-
+  findall(muchie(V0,V2,Categorie2,Categorii,[Analiza|Analize]), membru(muchie(V0,V1,Categorie2,[Categorie1|Categorii],Analize),Harta),Muchii).
+muchii_noi(muchie(V1,V2,Categorie1,[Categorie2|Categorii],Analize),Harta,Muchii) :-
   init_activ(Categorie2, V2, Muchii1),
-  findall(muchie(V1,V3,Categorie1,Categorii),membru(muchie(V2,V3,Categorie2,[]),Harta),Muchii2),
+  findall(muchie(V1,V3,Categorie1,Categorii,[Analiza|Analize]),membru(muchie(V2,V3,Categorie2,[],Analiza),Harta),Muchii2),
   append(Muchii1,Muchii2,Muchii).
 
 parse(Cat,Sir) :-
   init_agenda(Sir,0,Harta1),
   init_activ(Cat,0,Agenda),
   extinde_muchii(Agenda,Harta1,Harta2),
-  membru(muchie(0,M,Cat,[]),Harta2),
+  membru(muchie(0,M,Cat,[],Analiza),Harta2),
+  mwrite(Analiza),
   N is M+1,
   \+(membru(muchie(_,N,_,_),Harta2)).
 
@@ -61,3 +62,11 @@ cuvant(n,elevi).
 cuvant(n,carte).
 cuvant(v,iubeste).
 cuvant(v,iubesc).
+
+mwrite(Arbore) :- oglinda(Arbore, Imagine), print(Imagine), nl.
+
+oglinda([],[]) :- !.
+oglinda(Atom,Atom) :- atomic(Atom).
+oglinda([X1|X2],Imagine) :- oglinda(X1,Y2),
+  oglinda(X2,Y1),
+  append(Y1,[Y2],Imagine).
